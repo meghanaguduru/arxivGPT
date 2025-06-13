0d53546d5884e7747ab3edcb29f01bd9fb21759a
meghanaguduru <meghanaguduru@gmail.com>
2025-06-13 08:32:36 +0000

    Modifications to eval dataset generation

--- arxivGPT/src/eval/prepare_eval_data.py
+++ arxivGPT/src/eval/prepare_eval_data.py
@@ -12,6 +12,7 @@ from prompts.prompt_tuned import prompt
 from google import genai
 
 # Settings
+api_key = os.getenv("GOOGLE_API_KEY")
 vector_db_path = "../../data/processed/first_vector_db"
 output_path = "../../data/evaluation/eval_data.jsonl"
 embedding_dim = 384
@@ -23,7 +24,7 @@ vector_db = VectorDBLangchain(dim=embedding_dim, index_path=vector_db_path)
 vector_db.load_db()
 
 # Google API to use larger models
-client = genai.Client(api_key="AIzaSyCkWPHToVcuhqiaoTJrKIK8NtI5WL3fkTw")
+client = genai.Client(api_key=api_key)
 
 # Get all stored docs
 all_docs = vector_db.db.docstore._dict.values()
@@ -43,7 +44,7 @@ with jsonlines.open(output_path, mode='w') as writer:
             qa_prompt = (
                 "You are given a sentence from a research paper.\n"
                 "Generate a question someone might ask based on it, "
-                "and also provide a brief, correct answer using only the given sentence.\n\n"
+                "and provide a brief, correct answer using ONLY the given sentence.\n\n"
                 f"Sentence: {sentence}\n\n"
                 "Question:"
             )
@@ -55,14 +56,27 @@ with jsonlines.open(output_path, mode='w') as writer:
             # print(qa_response.text)
             qa_response = llm.pipeline.invoke(qa_prompt)
 
-            # Split into question and answer
-            if "Answer:" in qa_response:
-                question_part, answer = qa_response.split("Answer:", 1)
-                question = question_part.replace("Question:", "").strip()
-                answer = answer.strip()
+            # Clean prompt leakage if it repeats the full prompt
+            if "Sentence:" in qa_response and "Question:" in qa_response:
+                qa_response = qa_response.split("Sentence:", 1)[-1].strip()
+
+            # Split properly into question and answer
+            if "Question:" in qa_response and "Answer:" in qa_response:
+                print(f"Writing data sample from response")
+                question_part = qa_response.split("Question:", 1)[-1].split("Answer:")[0].strip()
+                answer_part = qa_response.split("Answer:", 1)[-1].strip()
+                question = question_part
+                answer = answer_part
             else:
                 print(f"[Warning] Skipping badly formatted response:\n{qa_response}")
                 continue
+                
+            print(f"""
+                paper_id: {i},
+                question: {question},
+                answer: {answer},
+                ground_context: {sentence}
+            """)
 
             # Write to file
             writer.write({
